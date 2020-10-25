cf87ebc8110e534b64b38034f6bade06c3b6ea36
blakcjack <alist.grouv@gmail.com>
2020-10-25 19:46:30 +0700

    Build the function of makeCacheMatrix. It will be able to: - set the value of the matrix and the value of the mean. If the value of the matrix changed, then the value of the mean will be set to NULL. - set the value of the matrix using the calculated one. - get the value of the matrix if it is ever computed - return the list of all those features.

--- cachematrix.R
+++ cachematrix.R
@@ -8,7 +8,18 @@
 ## makeCacheMatrix function will create special matrix object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set,
+       get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
