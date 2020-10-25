8bfa830d958be9b3c10dd25c53c9450c0e4e14b7
blakcjack <alist.grouv@gmail.com>
2020-10-25 19:47:38 +0700

    Using 8 indentations instead of 2.

--- cachematrix.R
+++ cachematrix.R
@@ -8,18 +8,18 @@
 ## makeCacheMatrix function will create special matrix object
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y){
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setsolve <- function(solve) m <<- solve
-  getsolve <- function() m
-  list(set = set,
-       get = get,
-       setmean = setmean,
-       getmean = getmean)
+        m <- NULL
+        set <- function(y){
+          x <<- y
+          m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set,
+             get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
 
 
